053c16423cbf7af1a37806f1ea55542f755ea096
Guillaume G <macadition@gmail.com>
2018-02-26 08:59:37 +0100

    Parts Theme Customisation

---
+++ src/Controller/TestController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class TestController extends AbstractController {
+
+    /**
+     * @Route("/cgv", name="app_cgv")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function cgv(){
+        return $this->render('pages/cgv.html.twig');
+    }
+
+    /**
+     * @Route("/cgu", name="app_cgu")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function cgu(){
+        return $this->render('pages/cgu.html.twig');
+    }
+
+    /**
+     * @Route("/", name="app_booking")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function booking(){
+        return $this->render('pages/booking.html.twig');
+    }
+
+    /**
+     * @Route("/", name="app_contact")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function contact(){
+        return $this->render('pages/contact.html.twig');
+    }
+
+}
