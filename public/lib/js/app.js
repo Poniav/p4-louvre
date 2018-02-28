913eb40fda600c935d8a387d80a0cbde90828b26
Guillaume G <macadition@gmail.com>
2018-02-28 03:57:01 +0100

    Update and added files CGU, CGV

--- public/lib/js/app.js
+++ public/lib/js/app.js
@@ -1,1 +1,35 @@
-console.log('hello');
+if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
+
+/*
+$(document).ready(function() {
+    var movementStrength = 25;
+    var height = movementStrength / $(window).height();
+    var width = movementStrength / $(window).width();
+    $(".jumbotron").mousemove(function(e){
+        var pageX = e.pageX - ($(window).width() / 2);
+        var pageY = e.pageY - ($(window).height() / 2);
+        var newvalueX = width * pageX * -1 - 200;
+        var newvalueY = height * pageY * -1 - 200;
+        $('.jumbotron').css("background-position", newvalueX+"px     "+newvalueY+"px");
+    });
+});*/
