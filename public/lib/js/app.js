448c3c1a67f4bd73e45aeb3a8c4ea5e5424ebc1d
Guillaume G <macadition@gmail.com>
2018-03-13 09:13:37 +0100

    Update booking view with datepicker and JS

--- public/lib/js/app.js
+++ public/lib/js/app.js
@@ -1,35 +1,78 @@
-if ($('#back-to-top').length) {
-    var scrollTrigger = 100, // px
-        backToTop = function () {
-            var scrollTop = $(window).scrollTop();
-            if (scrollTop > scrollTrigger) {
-                $('#back-to-top').addClass('show');
-            } else {
-                $('#back-to-top').removeClass('show');
-            }
-        };
-    backToTop();
-    $(window).on('scroll', function () {
-        backToTop();
-    });
-    $('#back-to-top').on('click', function (e) {
-        e.preventDefault();
-        $('html,body').animate({
-            scrollTop: 0
-        }, 700);
-    });
-}
+$(document).ready(function () {
 
-/*
-$(document).ready(function() {
-    var movementStrength = 25;
-    var height = movementStrength / $(window).height();
-    var width = movementStrength / $(window).width();
-    $(".jumbotron").mousemove(function(e){
-        var pageX = e.pageX - ($(window).width() / 2);
-        var pageY = e.pageY - ($(window).height() / 2);
-        var newvalueX = width * pageX * -1 - 200;
-        var newvalueY = height * pageY * -1 - 200;
-        $('.jumbotron').css("background-position", newvalueX+"px     "+newvalueY+"px");
+    var a = '#back-to-top';
+
+    if ($(a).length) {
+        var scrollTrigger = 100, // px
+            backToTop = function () {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    $('#back-to-top').addClass('show');
+                } else {
+                    $('#back-to-top').removeClass('show');
+                }
+            };
+        backToTop();
+        $(window).on('scroll', function () {
+            backToTop();
+        });
+        $(a).on('click', function (e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 700);
+        });
+    }
+
+    $.fn.datepicker.dates['fr'] = {
+        days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+        daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+        daysMin: ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa"],
+        months: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+        monthsShort: ["Jan", "Fév", "Mar", "Avr", "Mai", "Jun", "Jui", "Aoû", "Sep", "Oct", "Nov", "Déc"],
+        format: "dd/mm/yyyy",
+        titleFormat: "MM yyyy",
+        weekStart: 0,
+    };
+
+    /*$('.datepicker').datepicker({
+        format: 'dd/mm/yyyy',
+        startDate: new Date(),
+        language: "fr",
+        todayHighlight: true,
+        multidate: false,
+        daysOfWeekDisabled: "0,2",
+        datesDisabled: ['01/05/2018', '01/11/2018', '25/12/2018']
+    });*/
+
+    $('.datepicker').datepicker({
+        format: "dd/mm/yyyy",
+        startDate: "new Date()",
+        language: "fr",
+        multidate: false,
+        keyboardNavigation: false,
+        // daysOfWeekDisabled: "0,2",
+        autoclose: true,
+        todayHighlight: true,
+        // datesDisabled: ['01/05/2018', '01/11/2018', '25/12/2018'],
+        toggleActive: true,
+        defaultViewDate: { year: 1977, month: 04, day: 25 }
     });
-});*/
+
+    $('.datepicker-tickets').datepicker({
+        format: "dd/mm/yyyy",
+        language: "fr",
+        multidate: false,
+        keyboardNavigation: false,
+        autoclose: true,
+        todayHighlight: true,
+        toggleActive: true,
+        startView: 'century'
+    });
+
+
+
+});
+
+
+
