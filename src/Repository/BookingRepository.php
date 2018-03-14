ca582ebfa35e651b67fb2045ebd028cc808e764f
Guillaume G <macadition@gmail.com>
2018-03-15 00:15:44 +0100

    Add Booking Repository with method return tickets by date

---
+++ src/Repository/BookingRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Booking;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Booking|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Booking|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Booking[]    findAll()
+ * @method Booking[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class BookingRepository extends ServiceEntityRepository
+{
+    /**
+     * BookingRepository constructor.
+     * @param RegistryInterface $registry
+     */
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Booking::class);
+    }
+
+    /**
+     * @param \DateTime $date
+     * @return mixed
+     */
+    public function findTicketsByDate(\DateTime $date)
+    {
+        return $this->createQueryBuilder('c')
+            ->leftJoin('c.tickets', 'tickets')
+            ->addSelect('count(tickets)')
+            ->where('c.date = :date')
+            ->setParameter('date', $date)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+}
