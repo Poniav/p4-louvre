818b7dcbf122951959a10493968278c776a4ff74
Guillaume G <macadition@gmail.com>
2018-03-24 14:46:23 +0100

    Tests unit testPriceTickets Close #8

--- tests/Services/OrderManagerTest.php
+++ tests/Services/OrderManagerTest.php
@@ -2,15 +2,54 @@
 
 namespace App\Tests\Services;
 
-use App\Services\OrderManager;
-use Doctrine\ORM\EntityManagerInterface;
-use PHPUnit\Framework\TestCase;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use App\Entity\Booking;
+use App\Entity\Tickets;
 
-class OrderManagerTest extends TestCase
+class OrderManagerTest extends AbstractOrder
 {
+
+    /**
+     * Test Price Tickets by type, date of booking, birth date and discount special ticket
+     * @dataProvider dataTicketProvider
+     * @param $expectedPrice
+     * @param $type
+     * @param $date
+     * @param $birth
+     * @param $discount
+     */
+    public function testSetPriceTickets($expectedPrice, $type, $date, $birth, $discount)
+    {
+        $orderManger = $this->getOrderManagerInstance();
+        $booking = new Booking();
+        $booking->setType($type);
+        $date = $this->createDateFromFormat($date);
+        $booking->setDate($date);
+        $ticket = new Tickets();
+        $birth = $this->createDateFromFormat($birth);
+        $ticket->setBirth($birth);
+        $ticket->setDiscount($discount);
+        $booking->addTicket($ticket);
+
+        $orderManger->setPriceTickets($booking);
+
+        $this->assertEquals($expectedPrice, $ticket->getPrice());
+    }
+
+    // Set the expected price for the age
+    // $expectedPrice - $type - $date - $birth - $discount
+    // type Booking FULLDAY or HALFDAY
+    // discount Ticket special price
+    public function dataTicketProvider()
+    {
+        return [
+            [16, 1, '29/03/2018', '17/03/1990', 0],
+            [5, 0, '29/03/2018', '17/03/1990', 1],
+            [0, 1, '07/08/2019', '26/01/2017', 0],
+            [4, 0, '18/12/2020', '04/05/2009', 0],
+            [12, 1, '10/09/2019', '21/06/1950', 0],
+        ];
+    }
+
 
     /**
      * Test if age return the good price
@@ -39,34 +78,4 @@ class OrderManagerTest extends TestCase
         ];
     }
 
-    /**
-     * @return OrderManager
-     */
-    private function getOrderManagerInstance()
-    {
-        $session = $this->getMockBuilder(SessionInterface::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $flashBag = $this->getMockBuilder(FlashBagInterface::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $route = $this->getMockBuilder(UrlGeneratorInterface::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $em = $this->getMockBuilder(EntityManagerInterface::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $stripekey = null;
-
-        /** @var SessionInterface $session */
-        /** @var FlashBagInterface $flashBag */
-        /** @var UrlGeneratorInterface $route */
-        /** @var EntityManagerInterface $em */
-        $orderManager = new OrderManager($session, $flashBag, $route, $em, $stripekey);
-        return $orderManager;
-    }
 }
